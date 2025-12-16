5f2dad64af8f9ac08d00fb2d5b89cc9f7ec4eda5
Salvatore Cielo <di52vum@uan1.hsn.simba.lrz.de>
2025-12-16 18:01:44 +0100

    Added lettering to nvidia-smi, also changed columns?

--- hwmon-nvidia.sh
+++ hwmon-nvidia.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 nvidia-smi -q -d POWER | grep -e Readi  -e Draw | \
-awk '(NR%3==1){prefix = "nvidia-smi_" $1"_"$2"_"}(NR%3!=1){value=$1" "$5"W"; print prefix value }'
+awk 'BEGIN{i=97}(NR%2==1){prefix = "nvidia-smi_" $1"_"$2"_"}(NR%2!=1){value=$1" "$4+0"W"; printf("%c_", i++); print  prefix value }'
 
 for x in {0..120}; do
   MPATH=/sys/class/hwmon/hwmon${x}/device/
 #for MPATH in $( ls /sys/class/hwmon/hwmon*/device/ ); do
-#  if [ -f $MPATH/power1_oem_info ]; then
+  if [ -f $MPATH/power1_oem_info ]; then
     if [ -f $MPATH/power1_average ]; then
-      echo -n $x: $(cat $MPATH/power1_oem_info | awk '{print "hwmon_" $1 "_" $2"_Average"}')
+      echo -n ${x}_$(cat $MPATH/power1_oem_info | awk '{print "hwmon_" $1 "_" $2"_Average"}')
       echo " " $(cat $MPATH/power1_average  | awk '{print ($1/1.0E6)}')W
     fi
     if [ -f $MPATH/power1_inst* ]; then
-      echo -n $x: $(cat $MPATH/power1_oem_info | awk '{print "hwmon_" $1 "_"$2"_Instantaneous"}')
+      echo -n ${x}_$(cat $MPATH/power1_oem_info | awk '{print "hwmon_" $1 "_"$2"_Instantaneous"}')
       echo " " $(cat $MPATH/power1_instant  | awk '{print ($1/1.0E6)}')W
     fi
-#  fi
+  fi
 done
